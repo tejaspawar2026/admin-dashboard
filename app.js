7f8c4a736b38d16414cdf2a346eba90e7080802e
Tejas <tejaspawar2026@gmail.com>
2024-04-05 09:18:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+modeToggler = body.querySelector(".toggle-switch");
+sidebar = body.querySelector("nav");
+sidebarToggler = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
+}
+
+modeToggler.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
+});
+
+sidebarToggler.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
+});
